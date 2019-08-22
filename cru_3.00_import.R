190bebb00d23fd86493ee476798775dc415c7b4e
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-08-22 13:15:56 +0200

    change to 3.00

--- cru_3.00_import.R
+++ cru_3.00_import.R
@@ -3,6 +3,8 @@ rm(list=ls())
 library(ncdf4)
 library(raster)
 
+setwd("C:/R/bachelorproject/cruimport")
+file.exists("cru_ts_3_10.1901.2009.tmp.dat.nc")
 cru_3 <- nc_open("cru_ts_3_10.1901.2009.tmp.dat.nc")
 
 
