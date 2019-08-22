407ee2b0500dacbacf298571c1adc4bdc07715f4
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-08-22 15:58:28 +0200

    Update cru_3.00_import.R

--- cru_3.00_import.R
+++ cru_3.00_import.R
@@ -4,10 +4,16 @@ library(ncdf4)
 library(raster)
 
 setwd("C:/R/bachelorproject/cruimport")
-file.exists("cru_ts_3_10.1901.2009.tmp.dat.nc")
-cru_3 <- nc_open("cru_ts_3_10.1901.2009.tmp.dat.nc")
+file.exists("cru_ts_3_00.1901.2006.tmp.nc")
+cru_3 <- nc_open("cru_ts_3_00.1901.2006.tmp.nc")
 
 
-print(cru_3)
+tmp <- brick("cru_ts_3_00.1901.2006.tmp.nc", varname="tmp")
 
-tmp <- brick("cru_ts_3_10.1901.2009.tmp.dat.nc", varname="tmp")
+tmp
+
+print(tmp)
+
+xFromCol(tmp)       #running from 179.75 to -179.75 --> because raster uses center of grids
+yFromRow(tmp)       #same her , from 89.75 to-89.75
+
