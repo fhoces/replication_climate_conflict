63cbaa146345ec855540c0409a526ce34d308aef
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-08-30 12:46:43 +0200

    import CRU 4.03

--- using_CRU_403.R
+++ using_CRU_403.R
@@ -2,4 +2,44 @@ rm(list = ls())
 
 setwd("C:/R/bachelorproject")
 
-## importing the CRU data v4.03
+## importing the CRU data v4.03 (all)
+
+#set path and file name
+
+ncpath <- "C:/R/bachelorproject/data/cru_data/"
+ncname <- "cru_ts4.03.1901.2018.tmp.dat"
+ncfname <- paste(ncpath,ncname, ".nc", sep = "")
+dname <- "tmp"
+
+#open netCDF file
+
+cru_all <- nc_open(ncfname)
+cru_all
+
+#get lon + lat
+
+lon <- ncvar_get(cru_all,"lon")
+nlon <- dim(lon)
+lat <- ncvar_get(cru_all,"lat")
+nlat <- dim(lat)
+
+print(c(lon,lat))
+
+# get time
+
+time <- ncvar_get(cru_all, "time")
+time
+
+tunits <- ncatt_get(cru_all, "time", "units")
+tunits
+nt <- dim(time)
+nt
+
+# get temperature
+
+tmp_array <- ncvar_get(cru_all, dname)
+dlname <- ncatt_get(cru_all, dname, "long_name")
+dunits <- ncatt_get(cru_all, dname, "units")
+fillvalue <- ncatt_get(cru_all, dname, "_FillValue")
+dim(tmp_array)
+
