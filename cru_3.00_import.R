763caa8b38fb0da116999e3c156034fb2b1984a1
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-08-30 12:14:00 +0200

    now using CRU v4.03

--- cru_3.00_import.R
+++ cru_3.00_import.R
@@ -2,12 +2,15 @@ rm(list=ls())
 
 library(ncdf4)
 library(raster)
+library(maps)
+library(rasterVis)
+library(maptools)
 
 setwd("C:/R/bachelorproject/cruimport")
 file.exists("cru_ts_3_00.1901.2006.tmp.nc")
 cru_3 <- nc_open("cru_ts_3_00.1901.2006.tmp.nc")
 
-
+print(cru_3)
 tmp <- brick("cru_ts_3_00.1901.2006.tmp.nc", varname="tmp")
 
 tmp
@@ -17,3 +20,4 @@ print(tmp)
 xFromCol(tmp)       #running from 179.75 to -179.75 --> because raster uses center of grids
 yFromRow(tmp)       #same her , from 89.75 to-89.75
 
+plot(tmp$X372)
---
+++ using_CRU_403.R
@@ -0,0 +1,5 @@
+rm(list = ls())
+
+setwd("C:/R/bachelorproject")
+
+## importing the CRU data v4.03
