17cce33831f6fdd2b6fa03b5cd44cc564058f125
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-09-04 18:57:34 +0200

    comparing iso3_years

--- comparison_to_original.R
+++ comparison_to_original.R
@@ -2,7 +2,21 @@
 
 rm(list=ls())
 
+library(compare)
+library(dplyr)
+
 setwd("C:/R/bachelorproject/")
 pre <- read.csv("./csv_files/country_pre_ann.csv")
 tmp <- read.csv("./csv_files/country_tmp_ann.csv")
 original <- read.csv("C:/Users/marcr/Downloads/climate_conflict_replication_(original)/climate_conflict_replication (original)/clim_conflict_for_R.csv")
+
+original_tmppre <- subset(original, select=c(countryisocode, year_actual, temp_all, prec_all ))
+original_tmppre <- original_tmppre %>% rename(iso3 = countryisocode, years = year_actual)
+
+isotimepre <- subset(pre, select = c(iso3, years))
+isotimetmp <- subset(tmp, select = c(iso3, years))
+compare(isotimepre, isotimetmp, allowAll = TRUE)
+
+isotimeoriginial <- subset(original_tmppre, select = c(countryisocode, year_actual))
+
+anti_join(original_tmppre, pre, by=c("iso3","years"))
