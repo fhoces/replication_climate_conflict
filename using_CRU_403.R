e6a4027c2680a7293e56893402e90862c4b73bdf
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-09-04 12:11:03 +0200

    importing precipitation

--- using_CRU_403.R
+++ using_CRU_403.R
@@ -212,4 +212,136 @@ view(country_tmp)
 # calculate yearly data
 
 country_tmp_ann <- cbind(iso3rep,country_tmp %>% group_by(iso3) %>% separate(months, into=c("years", "months")) %>% group_by(years)%>% summarise(tmp=mean(tmp)))
-country_tmp_ann
+
+### temperature finished
+
+write.csv(country_tmp_ann, "C:/R/bachelorproject/csv_files/country_tmp_ann.csv")
+
+##importing precipitation
+
+ncpath <- "C:/R/bachelorproject/data/cru_data/"
+ncname <- "cru_ts4.03.1901.2018.pre.dat"
+ncfname <- paste(ncpath,ncname, ".nc", sep = "")
+dname <- "pre"
+
+#open netCDF file
+
+cru_all_pre <- nc_open(ncfname)
+cru_all_pre
+d
+# get precipitation
+
+pre_array <- ncvar_get(cru_all_pre, dname)
+dunits <- ncatt_get(cru_all_pre, dname, "units")
+fillvalue <- ncatt_get(cru_all_pre, dname, "_FillValue")
+dim(pre_array)
+
+nc_close(cru_all_pre)
+
+# replace netCDF fillvalues with NA's
+
+pre_array[pre_array==fillvalue$value] <- NA
+head(pre_array)
+
+
+## create the relevant data frame -- reshape data
+
+#reshape the array into a vector
+
+pre_vec <- as.vector(pre_array)
+length(pre_vec)
+
+#reshape this vector into a matrix
+
+pre_mat <- matrix(pre_vec, nrow = nlon*nlat, ncol=nt)
+dim(pre_mat)
+
+pre_all_df <- data.frame(cbind(lonlat, pre_mat))
+
+rm(pre_array)
+rm(pre_vec)
+rm(pre_mat)
+
+#rename col names
+
+names(pre_all_df)[1:2] <- paste(c("lon","lat"))
+names(pre_all_df)[3:ncol(pre_all_df)] <- paste(rep(years, each=12), rep(month, times=118))
+
+str(pre_all_df)
+
+#drop irrelevant columns , leaving only 1981-2002
+
+pre_red_df <- pre_all_df %>% select(!!removecols) #_red_ stands for reduced, we now have a dataframe of the tmp at the time we need: 1981-2002
+head(tmp_red_df)
+head(na.omit(tmp_red_df))
+
+rm(pre_all_df)
+
+##get pre data for the single countries
+
+#delete irrelevant grids in the pre_red_df dataframe
+#we see in gadmshape0afr for which extent we need data for
+
+pre_redafr_df <- subset(pre_red_df, lon >= -25.75 & lon <=63.75 & lat >= -40.75 & lat <= 37.75)
+head(pre_redafr_df)
+
+#rename the rows
+row.names(pre_redafr_df) <- c(1:28440)
+
+#turn pre_red_df into Spatialpoints
+
+pre_coords <- cbind(pre_redafr_df$lon, pre_redafr_df$lat)
+pre_pts <- SpatialPoints(coords = pre_coords, proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs"))
+pre_pts
+
+#get the location for the grid cells in pre dataframe
+
+loc_pre <- pre_pts %over% gadmshape0afr
+na.omit(loc_pre)
+summary(loc_pre)
+
+##merging the precipitation (incl. lon +lat) with the country codes
+
+str(loc_pre)
+str(pre_redafr_df)
+head(na.omit(loc_pre))
+
+full_pre <- bind_cols(loc_pre, pre_redafr_df)
+dim(full_pre)
+
+#delete obs. with either no pre-data or not defined for an african country
+
+full_pre <- na.omit(full_pre)
+dim(full_pre)
+head(full_pre)
+
+#NAME_0 is unnecessary, and we won't need lon and lat anymore
+
+full_pre <- full_pre %>% select(-NAME_0, -lon, -lat)
+
+
+## the precipitation is first averaged over the grid cells in a country, then over the month of a year
+
+
+#averaging over cells in country
+
+country_pre <- aggregate(full_pre[2:265], list(full_pre$GID_0), mean)
+names(country_pre)[1] <- "iso3"
+country_pre
+
+# restructuring the table -> we want the pre information for the month not in columns but in one column , each obs. being a row
+
+country_pre_num <- subset(country_pre, select = -iso3) #need this subset with only the numerical values to use function ; _num stands for numerical
+pre_vec1 <- as.vector(t(country_pre_num))
+length(pre_vec1)
+
+country_pre <- data.frame(iso3rep,colnames1,pre_vec1) 
+colnames(country_pre) <- c("iso3","months", "pre") 
+view(country_pre)
+
+# calculate yearly data
+
+country_pre_ann <- cbind(iso3rep,country_pre %>% group_by(iso3) %>% separate(months, into=c("years", "months")) %>% group_by(years)%>% summarise(pre=mean(pre)))
+
+### precipitation finished
+
