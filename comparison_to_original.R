4e566857186dd70140481345a9e3d49ac596957b
marcr94 <51209350+marcr94@users.noreply.github.com>
2019-09-18 17:45:41 +0200

    don't need that first one no more + new prepare

--- comparison_to_original.R
+++ comparison_to_original.R
@@ -1,111 +1,36 @@
-## we're comparing my results to the original files
-## country observations
+##############################################################################################
+rm(list=ls())
+
+library(foreign)
 library(dplyr)
 library(compare)
 library(tidyverse)
 
-rm(list = ls())
-
-setwd("C:/R/bachelorproject/climate_conflict_replication_(original)")
-climconf <- read_csv("./clim_conflict_for_R.csv")
-
-unique(climconf$countryisocode)
-
-iso3afralternative <- c("DZA","AGO","BEN","BWA","BFA","BDI","CMR","CPV","CAF","COM","COD","DJI","EGY",
-                        "GNQ","ERI","ETH","GAB","GMB","GHA","GIN","GNB","CIV","KEN","LSO","LBR","LBY",
-                        "MDG","MWI","MLI","MRT","MUS","MAR","MOZ","NAM","NER","NGA","COG","RWA","SHN",
-                        "STP","SEN","SYC","SLE","SOM","ZAF","SSD","SDN","SWZ","TZA","TGO","TUN","UGA",
-                        "ZMB","ZWE")
-str(iso3afraltive)
-iso3afrburke <- as.character(unique(climconf$countryisocode))
-countrynamesafrburke <- as.character(unique(climconf$country))
-countrynamesafrburke
-
-iso3afralternative[!iso3afralternative %in% iso3afrburke]
-iso3afrburke[!iso3afrburke %in% iso3afralternative]
-
-
-##
-rm(list=ls())
-library(foreign)
-
-setwd("C:/R/bachelorproject/")
-pre <- read.csv("./csv_files/country_pre_ann.csv",stringsAsFactors = FALSE)
-tmp <- read.csv("./csv_files/country_tmp_ann.csv",stringsAsFactors = FALSE)
-original <- read.csv("./climate_conflict_replication_(original)/climate_conflict.dta",
-                     stringsAsFactors = FALSE)
-
-original_tmppre <- subset(original, select=c(countryisocode, year_actual, temp_all, prec_all ))
-original_tmppre <- original_tmppre %>% rename(iso3 = countryisocode, years = year_actual)
-
-## finding rows that are in the original replication files, but not my tables
-
-anti_join(original_tmppre, pre, by=c("iso3","years"))
-
-#the results is : data for period 2003-2006 for multiple countries --> that's ok for now
-# + TCD and ZAR data for whole period -> not ok! why ?
-
-#answer : ZAR is democratic republic of Congo , former ZAIRE --> in my data COD
-#TCD Chad -> what's substitute ? let's check other way around
-
-anti_join(pre, original_tmppre, by=c("iso3","years"))
-#ok this get's messy
-
-# whole period:
-# COD : democratic republic of congo -> is ZAR in other sample
-# COM: comoros
-# dza: algeria
-# egy: egypt
-# eri: eritrea
-# esh: western sahara -> disputed
-# GNQ: equatorial giunea
-# LBY: libya
-# MAR: morocco
-# MUS: mauritius
-# NAM: namibia
-# REU: Reunion -> part of France
-# SSD: republic of south sudan
-# TUN: tunisia
-# 
-#only part period: AGO
-
-# change ZAIRE to COD in original data
-
-original_tmppre$iso3[original_tmppre$iso3 =="ZAR"] <- "COD"
-
-# possible reasons for all other isos: no conflict data ? 
-
-#let's import some conflict data
-
-###
-rm(list=ls())
-
-library(foreign)
 setwd("C:/R/bachelorproject")
 climate_conflict <- read_csv("./csv_files/climate_conflict.csv")
 climate_conflict_original <- read.dta("./climate_conflict_replication_(original)/climate_conflict.dta")
 
 yearsvector <- c(1981:2002)
-years <- climate_conflict_original %>% filter(year_actual %in% yearsvector)
+climate_conflict_original <- climate_conflict_original %>% filter(year_actual %in% yearsvector)
 
-unique(years$year_actual) #22 unique
-unique(years$countryisocode) #41 unique
+uniqueN(climate_conflict_original$year_actual) #22 unique
+uniqueN(climate_conflict_original$countryisocode) #41 unique
 
 # --> why only 889 obs? 
 
-years <- years %>% select(c("year_actual", "countryisocode", "temp_all", "prec_all", "war_prio_new"))
-years <- years %>% rename("years" = "year_actual", "iso3" = "countryisocode")
-anti_join(climate_conflict, years)
+climate_conflict_original <- climate_conflict_original %>% select(c("year_actual", "countryisocode", "temp_all", "prec_all", "war_prio_new", "gdp", "polity2"))
+climate_conflict_original <- climate_conflict_original %>% rename("years" = "year_actual", "iso3" = "countryisocode")
+anti_join(climate_conflict, climate_conflict_original, by = c("iso3", "years"))
 
 #rename ZAR to COD, then only 13 obs. missing in original replication file (why?)
 
-years$iso3[years$iso3 == "ZAR"] <- "COD" 
+climate_conflict_original$iso3[climate_conflict_original$iso3 == "ZAR"] <- "COD" 
 
-anti_join(climate_conflict, years) #better
+anti_join(climate_conflict, climate_conflict_original, by = c("iso3", "years")) #better , only namibia and 3 years of angola missing
 
 ##join tables
 
-clim_conf_compare <- full_join(years, climate_conflict)
+clim_conf_compare <- full_join(climate_conflict_original, climate_conflict, by = c("years", "iso3"))
 
 ##compare conflict, tmp and pre 
 
@@ -113,6 +38,9 @@ clim_conf_compare <- full_join(years, climate_conflict)
 clim_conf_compare <- clim_conf_compare %>% mutate(conflict_diff = case_when(war_prio_new == conflict ~ 0, TRUE ~ 1))
                   
 table(clim_conf_compare$conflict_diff) # we have 761 equal values and 141 where conflict is reported differently
+
+length(clim_conf_compare$war_prio_new[is.na(clim_conf_compare$war_prio_new)]) # 13 missing 
+length(clim_conf_compare$conflict[is.na(clim_conf_compare$conflict)])
 
 #let's see where is more conflict reported
 
@@ -122,16 +50,17 @@ clim_conf_compare <- clim_conf_compare %>% mutate(conflict_diff = case_when(war_
 
 table(clim_conf_compare$conflict_diff) # -1 = 128 , 0 = 761, 1 = 0
 
-# --> only our conflict variable is overreported compared to origianl replication set !
-# let's see where
+# --> only our conflict variable is overreported compared to original replication set !
+# let's see where..
 
-conflict_diff_table <- clim_conf_compare %>% filter(conflict != war_prio_new) %>% select(years, iso3, war_prio_new, countryname, conflict, conflict_diff)
+conflict_diff_table <- clim_conf_compare %>% 
+  filter(conflict != war_prio_new) %>% select(years, iso3, war_prio_new, countryname, conflict, conflict_diff)
 
 view(conflict_diff_table)
 
 #let's research these in PRIO conflict table
 ## --> coding of this variable in the original replication file does not make any sense to me!!
-
+## 
 ### now compare tmp and pre
 
 clim_conf_compare <- clim_conf_compare %>% mutate(tmp_diff = temp_all - tmp,
